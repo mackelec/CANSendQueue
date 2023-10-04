1bbca88b32614ba5b99387cdf356924744d23b79
mackelec <mackelec@users.noreply.github.com>
2023-10-05 07:44:56 +1000

    Update CANSendQueue.h

--- CANSendQueue.h
+++ CANSendQueue.h
@@ -11,7 +11,11 @@ struct CANMessage {
 
 class CANSendQueue {
 public:
-  CANSendQueue();
+  // Define the function signature for the sendQCANMessage function
+  typedef bool (*SendFunction)(uint16_t id, uint8_t* array, uint8_t length);
+
+  CANSendQueue(SendFunction sendFunc, int customQueueSize = defaultQueueSize);
+  ~CANSendQueue();
 
   bool enqueue(const CANMessage& msg);
   void setMaxRetries(uint8_t retries);
@@ -20,12 +24,14 @@ public:
   void Send();
 
 private:
-  static const int queueSize = 10;
-  CANMessage queue[queueSize];
-  uint8_t retryCount[queueSize];
+  static const int defaultQueueSize = 10;
+  int queueSize;
+  CANMessage* queue;
+  uint8_t* retryCount;
   int front, rear;
   uint32_t failedCount;
   uint8_t maxRetries;
+  SendFunction sendFunction; // Store the function pointer
 
   bool isFull() const;
   bool isEmpty() const;
